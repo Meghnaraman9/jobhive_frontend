81298e8ef33608af4426fbb2fb42d2cc15f53157
Meghna-97 <meghanaraman9@gmail.com>
2026-05-14 22:32:47 +0530

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-    base: "/jobhive_frontend/",
-=======
-    base:"/jobhive_frontend/",
->>>>>>> f8948b6 (github pages setup)
-
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "/jobhive_frontend/",
 });
